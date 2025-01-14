bdca38f92f56efb2b9d4f38ae3a534895d7e466b
Apollo59 <jackmccormack2008@gmail.com>
2025-01-14 09:15:42 -0500

    oh fuck when was the last save

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from pygame.locals import *
 import pygame.freetype
 from random import randint
+import time
 
 pygame.init()
 pygame.display.init()
@@ -18,6 +19,12 @@ intro = 0
 agreers = 0
 diffselect = 0
 diff = 0
+dissrate = 0
+angerpercent = 0
+townspeople = 100
+pow1timeout = 0
+message = ""
+
 
 agreersSTR = str(agreers)
 
@@ -36,6 +43,23 @@ backgroundcolor = (40, 40, 40)
 
 text = GAME_FONT.render(agreersSTR, True, red, blue)
 textRect = text.get_rect()
+
+
+#ActualThings
+
+def Power2():
+    print("Nothing Yet")
+
+
+
+
+def lose():
+    screen.fill(white)
+    loserender = GAME_FONT.render("YOU LOSE", True, black)
+    screen.blit(loserender, (500, 325))
+
+
+
 
 #Main Loop
 while running:
@@ -65,26 +89,55 @@ while running:
                 diff = 0
                 diffselect = 1
                 agreers = 75
+                dissrate = 150
             if keys[pygame.K_1]:
                 diff = 1
                 diffselect = 1
                 agreers = 50
+                dissrate = 300
             if keys[pygame.K_2]:
                 diff = 2
                 diffselect = 1
                 agreers = 25
+                dissrate = 450
             if keys[pygame.K_3]:
                 diff = 3
                 diffselect = 1
                 agreers = 0
+                dissrate = 500
             pygame.display.update()
 
     if diffselect != 0:
         if splashscreen == 0:    #Draw
             screen.fill(backgroundcolor)
-            fontrender = GAME_FONT.render(agreersSTR, True, white)
+            fontrender = GAME_FONT.render(agreersSTR + "/100", True, white)
             screen.blit(fontrender, (0, 0))
+            lastmessagerender = GAME_FONT.render(message, True, white)
+            screen.blit(lastmessagerender, (0, 100))
             agreersSTR = str(agreers)
+            if keys[pygame.K_a]:
+                if pow1timeout == 0:
+                    print("Lying to people")
+                    message = "Oil is good for you."
+                    agreers = agreers + 3
+                    pow1timeout = 300
+            if pow1timeout > 0:
+                pow1timeout = pow1timeout - 1
+            if dissrate == 600:
+                angerpercent = 1
+                dissrate = 0
+            else:
+                angerpercent = 0
+                dissrate = dissrate + 1
+            if angerpercent == 1:
+                agreers = agreers - 1
+            if agreers == 0:
+                lose()
+                dissrate = 0
+
+#Dissenter Chance
+
+
             pygame.display.update()
             print(agreers)
             clock.tick(60)
--- readme.md
+++ readme.md
@@ -10,8 +10,8 @@
     Difficulty Slider     (Added)
     Convert Townspeople
     Win??
-    Passivly Goes down to overthrow you
-    You are a king
+    Passivly Goes down to overthrow you        (Done)
+    You are a king                          (IDK why this is in here)
     You have to get the townspeople to agree to policy which would raise cliamte change
     Enemies give actual facts, you spew retoric
 
